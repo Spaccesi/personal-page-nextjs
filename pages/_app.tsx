aa8feefcd243338aa0f8716b9cf85701f48e4147
Agus S <agustin@spaccesi.com>
2022-09-06 22:54:47 +0200

    env variable changed

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,10 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Script from 'next/script'
 
-const isProduction = process.env.NEXT_PUBLIC_ENV === "production";
-
 function MyApp({ Component, pageProps }: AppProps) {
   return (
   <>
-    {isProduction && (
+    {process.env.NEXT_PUBLIC_ENV == "production" && (
       <>
         <Script
             src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTIC_ID}`}
