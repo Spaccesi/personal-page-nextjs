ef6b8f6a8613041a7f782b64e95b19c9df3dc3ff
Agus S <agustin@spaccesi.com>
2022-09-06 18:19:33 +0200

    GA only in prod

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,23 +2,29 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Script from 'next/script'
 
+const isProduction = process.env.NEXT_PUBLIC_PROD === "production";
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
   <>
-    <Script
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTIC_ID}`}
-        strategy="lazyOnload"
-      />
-      <Script id="google-analytics" strategy="lazyOnload">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){window.dataLayer.push(arguments);}
-          gtag('js', new Date());
+    {isProduction && (
+      <>
+        <Script
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTIC_ID}`}
+            strategy="lazyOnload"
+          />
+          <Script id="google-analytics" strategy="lazyOnload">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){window.dataLayer.push(arguments);}
+              gtag('js', new Date());
 
-          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTIC_ID}');
-        `}
-    </Script>
-
+              gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTIC_ID}');
+            `}
+        </Script>
+      </>
+    )}
+    
     <Component {...pageProps} />
   </>
   )
