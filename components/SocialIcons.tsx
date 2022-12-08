095a21a20f4c313db309cb22529629e465164874
Agustin Spaccesi <agustin@MacBook-Pro-de-Agustin.local>
2022-12-08 23:56:42 +0100

    Particles adapted to screen size

--- components/SocialIcons.tsx
+++ components/SocialIcons.tsx
@@ -1,14 +1,22 @@
-import { FaLinkedinIn } from "react-icons/fa";
+import { SiLinkedin, SiInstagram, SiWhatsapp, SiGithub } from "react-icons/si";
+import {HiMail} from 'react-icons/hi'
+import Link from "next/link";
 
 const SocialIcons = () => {
   return (
-    <div className="columns-1 absolute bottom-5 right-5">
-      <div className="bubble-button">
-        <FaLinkedinIn />
-      </div>
-      <div className="mt-4 bubble-button">
-        <FaLinkedinIn />
-      </div>
+    <div className='grid-rows-1 grid-flow-col grid gap-8 md:justify-start justify-end'>
+      <Link href={'https://www.linkedin.com/in/agustinspaccesi/'}>
+        <SiLinkedin size={24}/>
+      </Link>
+      <Link href={'https://github.com/Spaccesi'}>
+        <SiGithub size={24}/>
+      </Link>
+      <Link href={'https://wa.me/34695496544'}>
+        <SiWhatsapp size={24}/>
+      </Link>
+      <Link href={'mailto:agustin@spaccesi.com'}>
+        <HiMail size={24}/>
+      </Link>
     </div>
   );
 }
