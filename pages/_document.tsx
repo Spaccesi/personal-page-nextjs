83f626d18f0957070f2c834b067c773c1838c436
Spaccesi <39572282+Spaccesi@users.noreply.github.com>
2026-03-20 15:02:33 +0100

    feat: add SEO component and sitemap with multi-language support

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,12 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 import Script from 'next/script'
+import type { DocumentProps } from 'next/document'
 
-export default function Document() {
+export default function Document(props: DocumentProps) {
+  const locale = props.__NEXT_DATA__.locale || 'en'
+
   return (
-    <Html>
+    <Html lang={locale}>
       <Head>
         <Script id='screen-size-mobile' strategy='beforeInteractive'>
           {
